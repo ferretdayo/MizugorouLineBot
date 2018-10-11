620a93c0bd162a4a102d146dc52839dbd12b778e
ferretdayo <ferretdayo@hotmail.co.jp>
2018-10-12 00:46:03 +0900

    i

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const client = new line.Client(config);
 
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
+    return Promise.reject(null);
   }
 
   return client.replyMessage(event.replyToken, {
