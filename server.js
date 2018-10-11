6522c787aa02b7195a4e83491dbd4af291ab0c58
ferretdayo <ferretdayo@hotmail.co.jp>
2018-10-12 01:02:00 +0900

    i

--- server.js
+++ server.js
@@ -22,24 +22,24 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
   // イベントオブジェクトを順次処理。
   req.body.events.forEach((event) => {
-      // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-      if (event.type == "message" && event.message.type == "text"){
-          // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-          if (event.message.text == "こんにちは"){
-              // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-              events_processed.push(client.replyMessage(event.replyToken, {
-                  type: "text",
-                  text: "これはこれは"
-              }));
-          }
+    // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+    if (event.type == "message" && event.message.type == "text"){
+      // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+      if (event.message.text == "こんにちは"){
+        // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+        events_processed.push(client.replyMessage(event.replyToken, {
+          type: "text",
+          text: "これはこれは"
+        }));
       }
+    }
   });
-
+  console.log("events_processed: ", events_processed)
   // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
   Promise.all(events_processed).then(
-      (response) => {
-          console.log(`${response.length} event(s) processed.`);
-      }
+    (response) => {
+      console.log(`${response.length} event(s) processed.`);
+    }
   );
 });
 
