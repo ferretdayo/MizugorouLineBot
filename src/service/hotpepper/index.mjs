5a2226d44ca7f7e098bffe3b9c1b954b697879c8
ferretdayo <ferretdayo@hotmail.co.jp>
2018-10-14 00:34:57 +0900

    一度の表示は10件までらしい

--- src/service/hotpepper/index.mjs
+++ src/service/hotpepper/index.mjs
@@ -2,12 +2,11 @@ import { http } from '../http'
 
 export default {
   searchFoodShop: async(keyword) => {
-    console.log("keyword", keyword)
     const res = await http.get('http://webservice.recruit.co.jp/hotpepper/gourmet/v1', {
       params: {
         key: process.env.HOTPEPPER_API_KEY,
         keyword: keyword,
-        count: 20,
+        count: 10,
         format: 'json'
       }
     })
